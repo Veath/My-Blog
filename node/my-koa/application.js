5e0b3ea73c7fcea3d0d33d7b537b24482023d15c
Veath <627846032@qq.com>
2019-02-25 15:08:49 +0800

    my-koa 基本结构

---
+++ node/my-koa/application.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const EventEmitter = require('events');
+const context = require('./context');
+const request = require('./request');
+const response = require('./response');
+
+class Koa extends EventEmitter {
+    constructor () {
+        super();
+    }
+    user () {
+
+    }
+    listen () {
+
+    }
+}
+
+module.exports = Koa;
