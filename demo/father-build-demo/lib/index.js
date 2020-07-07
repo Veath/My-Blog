ccfdbc56276b3f5a8a68e77a30f4fc0ec18cda94
Veath <627846032@qq.com>
2020-07-07 18:47:41 +0800

    add father-demo

---
+++ demo/father-build-demo/lib/index.js
@@ -0,0 +1,34 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+function _react() {
+  const data = _interopRequireDefault(require("react"));
+
+  _react = function _react() {
+    return data;
+  };
+
+  return data;
+}
+
+function _module() {
+  const data = _interopRequireDefault(require("module"));
+
+  _module = function _module() {
+    return data;
+  };
+
+  return data;
+}
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _default = () => {
+  _module().default._findPath('demo', ['demo'], true);
+};
+
+exports.default = _default;
