2b52b78807ccb6c31388c6f3752c13d3a64ca000
Veath <627846032@qq.com>
2021-02-26 14:34:56 +0800

    feat: add lazyMan

--- fragmentary/数组排序组合.js
+++ fragmentary/数组排序组合.js
@@ -104,3 +104,233 @@ let formats = [
 ];
 
 console.log(JSON.stringify(combination(formats)));
+
+let names = ['iPhone X', 'iPhone XS'];
+
+let colors = ['黑色', '白色'];
+
+let storages = ['64g', '256g'];
+
+let combine = function (...chunks) {
+  let res = [];
+
+  let helper = function (chunkIndex, prev) {
+    let chunk = chunks[chunkIndex];
+    let isLast = chunkIndex === chunks.length - 1;
+    for (let val of chunk) {
+      let cur = prev.concat(val);
+      if (isLast) {
+        // 如果已经处理到数组的最后一项了 则把拼接的结果放入返回值中
+        res.push(cur);
+      } else {
+        helper(chunkIndex + 1, cur);
+      }
+    }
+  };
+
+  // 从属性数组下标为 0 开始处理
+  // 并且此时的 prev 是个空数组
+  helper(0, []);
+
+  return res;
+};
+
+console.log(combine(names, colors, storages));
+
+/**
+ * 
+输入: n = 4, k = 2
+输出:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+ */
+let combine = function (n, k) {
+  let ret = [];
+
+  let helper = (start, prev) => {
+    let len = prev.length;
+    if (len === k) {
+      ret.push(prev);
+      return;
+    }
+
+    // 还有 rest 个位置待填补
+    let rest = k - prev.length;
+    for (let i = start; i <= n; i++) {
+      if (n - i + 1 < rest) {
+        continue;
+      }
+      helper(i + 1, prev.concat(i));
+    }
+  };
+  helper(1, []);
+  return ret;
+};
+
+console.log(combine(4, 2));
+
+/** 可以重复使用里面的数字
+  * 示例 1:
+
+输入: candidates = [2,3,6,7], target = 7,
+所求解集为:
+[
+  [7],
+  [2,2,3]
+]
+示例 2:
+
+输入: candidates = [2,3,5], target = 8,
+所求解集为:
+[
+  [2,2,2,2],
+  [2,3,3],
+  [3,5]
+]
+  */
+let combinationSum = function (candidates, target) {
+  let res = [];
+
+  let helper = (start, prevSum, prevArr) => {
+    // 由于全是正整数 所以一旦和大于目标值了 直接结束本次递归即可。
+    if (prevSum > target) {
+      return;
+    }
+    // 目标值达成
+    if (prevSum === target) {
+      res.push(prevArr);
+      return;
+    }
+
+    for (let i = start; i < candidates.length; i++) {
+      // 这里还是继续从start本身开始 因为多个重复值是允许的
+      let cur = candidates[i];
+      let sum = prevSum + cur;
+      let arr = prevArr.concat(cur);
+      helper(i, sum, arr);
+    }
+  };
+
+  helper(0, 0, []);
+
+  return res;
+};
+
+/**
+ * 不可以重复使用数字
+ * 示例 1:
+
+输入: candidates = [10,1,2,7,6,1,5], target = 8,
+所求解集为:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+示例 2:
+
+输入: candidates = [2,5,2,1,2], target = 5,
+所求解集为:
+[
+  [1,2,2],
+  [5]
+]
+ */
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+function combinationSum(list, target) {
+  const result = [];
+  const cache = new Set();
+
+  const l = list.sort();
+
+  const loop = (start, sum, preArr) => {
+    if (sum > target) {
+      return;
+    }
+
+    if (cache.has(preArr.toString())) {
+      return;
+    }
+
+    if (sum === target) {
+      result.push(preArr);
+      cache.add(preArr.toString());
+    }
+
+    for (let i = start; i < l.length; i++) {
+      loop(i + 1, sum + l[i], [...preArr, l[i]]);
+    }
+  };
+
+  loop(0, 0, []);
+  return result;
+}
+
+/**
+ * 输入: [1,2,2]
+输出:
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function (nums) {
+  let n = nums.length;
+  let res = [];
+  if (!n) {
+    return res;
+  }
+
+  nums.sort();
+
+  let used = {};
+
+  let helper = (start, prev, target) => {
+    if (prev.length === target) {
+      let key = genKey(prev);
+      if (!used[key]) {
+        res.push(prev);
+        used[key] = true;
+      }
+      return;
+    }
+
+    for (let i = start; i < n; i++) {
+      let rest = n - i;
+      let need = target - prev.length;
+      if (rest < need) {
+        continue;
+      }
+      helper(i + 1, prev.concat(nums[i]), target);
+    }
+  };
+
+  for (let i = 1; i <= n; i++) {
+    helper(0, [], i);
+  }
+
+  return [[], ...res];
+};
+
+function genKey(arr) {
+  return arr.join('~');
+}
